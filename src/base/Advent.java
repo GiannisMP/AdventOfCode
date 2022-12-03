7703701d3dae6f03874f9f40c7035a6ba1895b3d
GiannisMP <it.john@hotmail.com>
2022-12-03 12:34:55 +0200

    Initial commit

---
+++ src/base/Advent.java
@@ -0,0 +1,19 @@
+package base;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public abstract class Advent {
+
+    protected List<String> lines;
+
+    public Advent() {
+        try {
+            lines = Files.readAllLines(Paths.get("src/" + getClass().getPackage().getName() + "/input"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
