0dd96c022747600fbd4f663928f5b1e59f0c6b70
i.bermperoglou <it.john@hotmail.com>
2022-12-08 15:31:15 +0200

    Made code nicer

--- src/day8/Tree.java
+++ src/day8/Tree.java
@@ -3,6 +3,7 @@ package day8;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Tree {
     enum DIRECTION { LEFT, RIGHT, TOP, BOTTOM }
@@ -19,21 +20,24 @@ public class Tree {
     }
 
     private boolean isVisibleFrom(Tree tree, DIRECTION direction) {
-        if (tree == null) return true;
-        return height > tree.height && isVisibleFrom(tree.treeMap.get(direction), direction);
+        return Objects.isNull(tree) || height > tree.height && isVisibleFrom(tree.treeMap.get(direction), direction);
     }
 
     public boolean isVisible() {
-        return Arrays.stream(DIRECTION.values()).map(direction -> isVisibleFrom(treeMap.get(direction), direction)).reduce(Boolean::logicalOr).orElse(true);
+        return Arrays.stream(DIRECTION.values())
+                .map(direction -> isVisibleFrom(treeMap.get(direction), direction))
+                .reduce(Boolean::logicalOr).orElse(true);
     }
 
     private int scenicFrom(Tree tree, DIRECTION direction, int current) {
-        if (tree == null) return current;
-        current++;
-        return height > tree.height ? scenicFrom(tree.treeMap.get(direction), direction, current) : current;
+        return Objects.isNull(tree)
+                ? current
+                : height > tree.height ? scenicFrom(tree.treeMap.get(direction), direction, ++current) : ++current;
     }
     public int scenic() {
-        return treeMap.keySet().stream().map(direction -> scenicFrom(treeMap.get(direction), direction, 0)).reduce((a,b) -> a * b).orElse(0);
+        return treeMap.keySet().stream()
+                .map(direction -> scenicFrom(treeMap.get(direction), direction, 0))
+                .reduce((a,b) -> a * b).orElse(0);
     }
 
 }
